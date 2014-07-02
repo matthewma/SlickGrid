9d9d833bae12494941c460e6754ec29aac890e53
matthewma <mxc0317@gmail.com>
2014-07-02 16:15:08 -0400

    Adding a missing parameter
    parameter d was missing, causing frozen columns not rendered when the viewport is scrolled relatively far right.

--- slick.grid.js
+++ slick.grid.js
@@ -1944,7 +1944,7 @@ if (typeof Slick === "undefined") {
                         appendCellHtml(stringArrayL, row, i, colspan, d);
                     }
                 } else if (( options.frozenColumn > -1 ) && ( i <= options.frozenColumn )) {
-                    appendCellHtml(stringArrayL, row, i, colspan);
+                    appendCellHtml(stringArrayL, row, i, colspan, d);
                 }
 
                 if (colspan > 1) {
